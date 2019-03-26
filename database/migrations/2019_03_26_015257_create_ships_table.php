f2c6d86ea27f310f1d8a9e91dcbf805fe8f830b8
ricardohenrique <ricardo.hb@outlook.com>
2019-03-26 00:44:43 -0300

    init migrations

---
+++ database/migrations/2019_03_26_015257_create_ships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ships', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('initials', 2);
+            $table->smallInteger('size_x');
+            $table->smallInteger('size_y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+}
