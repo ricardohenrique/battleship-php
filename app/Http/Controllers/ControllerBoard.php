37bc497701002406360e3ebdc1bb324befcd179b
ricardohenrique <ricardo.hb@outlook.com>
2019-03-26 00:46:47 -0300

    init controller board

---
+++ app/Http/Controllers/ControllerBoard.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RequestBoardShip;
+use App\Services\ServiceBoard;
+
+class ControllerBoard extends Controller
+{
+    /**
+     * @var ServiceBoard
+     */
+    public $service;
+
+    /**
+     * ControllerBoard constructor.
+     * @param ServiceBoard $service
+     */
+    public function __construct(ServiceBoard $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        return response()->json($this->service->getAll());
+    }
+
+    public function show($id)
+    {
+        return response()->json($this->service->getById($id));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        return response()->json($this->service->createBoard());
+    }
+
+    /**
+     * @param RequestBoardShip $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function putShip(RequestBoardShip $request, $boardId, $shipId)
+    {
+        return response()->json($this->service->setShipOnBoard($boardId, $shipId, $request->all()));
+    }
+}
